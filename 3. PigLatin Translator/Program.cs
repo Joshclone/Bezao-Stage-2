720ff8f4272cd1a431bbd204d3d617ecd57b770b
Nwaeze Joshua <nwaezejoshua@gmail.com>
2022-09-06 08:48:13 +0100

    Update Program.cs

--- 3. PigLatin Translator/Program.cs
+++ 3. PigLatin Translator/Program.cs
@@ -6,13 +6,15 @@ namespace PigLatinTranalator
     {
         /*
         PigLatin Translator Rules:
-
-        1. If a word starts with a consonant and a vowel, put the first letter of the word at the end of the word and add "ay."
-        Example: Happy = appyh + ay = appyhay
-        2. If a word starts with two consonants move the two consonants to the end of the word and add "ay."
-        Example: Child = Ildch + ay = Ildchay
-        3. If a word starts with a vowel add the word "way" at the end of the word.
+        1. If a word starts with a vowel add the word "way" at the end of the word.
         Example: Awesome = Awesome +way = Awesomeway
+       
+        2. If a word starts with a consonant and a vowel, put the first letter of the word at the end of the word and add "ay."
+        Example: Happy = appyh + ay = appyhay
+       
+        3. If a word starts with two consonants move the two consonants to the end of the word and add "ay."
+        Example: Child = ildch + ay = ildchay
+        
         */
 
         static void Main(string[] args)
